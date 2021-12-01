d88ef770d0cfc57ff4c706139810e520bdc7c07b
Brinda3 <88205782+Brinda3@users.noreply.github.com>
2021-12-01 18:55:55 +0530

    Update code.py

--- code.py
+++ code.py
@@ -87,10 +87,34 @@ def get_repo_info(h1_tag, star_tag):
     stars = parse_star_count(star_tag.text.strip())
     return username, repo_name, stars, repo_url
 
+def get_repo_info(h3_tag, star_tag):
+    # returns all the required info about a repository
+    a_tags = h3_tag.find_all('a')
+    username = a_tags[0].text.strip()
+    repo_name = a_tags[1].text.strip()
+    repo_url =  base_url + a_tags[1]['href']
+    stars = parse_star_count(star_tag.text.strip())
+    return username, repo_name, stars, repo_url
+
 # get_repo_info gives us the extracted username, repo_name, starts, repo_url
 def get_topic_repos(topic_doc):#function for h3 class
     h3_selection_class = "f3 color-fg-muted text-normal lh-condensed"
     repo_tags = topic_doc.find_all('h3',{'class': h3_selection_class})
+    
+     # Get star tags
+    star_tags = topic_doc.find_all('a', { 'class': 'social-count float-none'})
+    
+    topic_repos_dict = { 'username': [], 'repo_name': [], 'stars': [],'repo_url': []}
+
+    # Get repo info
+    for i in range(len(repo_tags)):
+        repo_info = get_repo_info(repo_tags[i], star_tags[i])
+        topic_repos_dict['username'].append(repo_info[0])
+        topic_repos_dict['repo_name'].append(repo_info[1])
+        topic_repos_dict['stars'].append(repo_info[2])
+        topic_repos_dict['repo_url'].append(repo_info[3])
+        
+    return pd.DataFrame(topic_repos_dict)
     
 def get_topic_repos(topic_doc):
     # Get the h1 tags containing repo title, repo URL and username
