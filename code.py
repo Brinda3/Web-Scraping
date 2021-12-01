f04d670647f30ee0ebc18a7e342879be8231c08a
Brinda3 <88205782+Brinda3@users.noreply.github.com>
2021-12-01 14:46:52 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,20 @@
+import os
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-# `get_topic_titles` can be used to get the list of titles
+
 def get_topics_page():
-    # TODO - add comments
     topics_url = 'https://github.com/topics'
     response = requests.get(topics_url)
     if response.status_code != 200:
         raise Exception('Failed to load page {}'.format(topic_url))
+    with open ('webpage.html','w') as f:
+        f.write(response.text)
     doc = BeautifulSoup(response.text, 'html.parser')
     return doc
 doc = get_topics_page()
+# get_topic_page can be used to download the page
+
 
 def get_topic_titles(doc):
     selection_class = 'f3 lh-condensed mb-0 mt-1 Link--primary'
@@ -19,14 +24,20 @@ def get_topic_titles(doc):
         topic_titles.append(tag.text)
     return topic_titles
 titles = get_topic_titles(doc)
+# get_topic_titles can be used to get the list of titles
+
+
+# Similarly we have defined functions for descriptions and URLs.
 def get_topic_descs(doc):
-    desc_selector = 'f5 color-text-secondary mb-0 mt-1'
+    desc_selector = "f5 color-fg-muted mb-0 mt-1"
     topic_desc_tags = doc.find_all('p', {'class': desc_selector})
     topic_descs = []
     for tag in topic_desc_tags:
-        topic_descs.append(tag.text.strip())
+        topic_descs.append(tag.text)
     return topic_descs
- # Similarly we have defined functions for descriptions and URLs. 
+desc = get_topic_descs(doc)
+# get_topic_descs function extracts the description of each topic
+
 def get_topic_urls(doc):
     topic_link_tags = doc.find_all('a', {'class': 'd-flex no-underline'})
     topic_urls = []
@@ -34,8 +45,11 @@ def get_topic_urls(doc):
     for tag in topic_link_tags:
         topic_urls.append(base_url + tag['href'])
     return topic_urls
-  
-  
+urls=get_topic_urls(doc)
+# get_topic_urls is to extract the urls of respective repositories
+
+
+# get_topic_urls is to extract the urls of respective repositories
 def scrape_topics():
     topics_url = 'https://github.com/topics'
     response = requests.get(topics_url)
@@ -48,8 +62,11 @@ def scrape_topics():
         'url': get_topic_urls(doc)
     }
     return pd.DataFrame(topics_dict)
-  
- def get_topic_page(topic_url):
+
+# so scrape_topicsfunction returns the extracted information
+
+# Get the top 25 repositories from a topic page
+def get_topic_page(topic_url):
     # Download the page
     response = requests.get(topic_url)
     # Check successful response
@@ -58,8 +75,9 @@ def scrape_topics():
     # Parse using Beautiful soup
     topic_doc = BeautifulSoup(response.text, 'html.parser')
     return topic_doc
- doc = get_topic_page('https://github.com/topics/3d')
+doc = get_topic_page('https://github.com/topics/3d')
 
+# To get the top repositories we can pick h1 tags and h3tags
 def get_repo_info(h1_tag, star_tag):
     # returns all the required info about a repository
     a_tags = h1_tag.find_all('a')
@@ -68,11 +86,17 @@ def get_repo_info(h1_tag, star_tag):
     repo_url =  base_url + a_tags[1]['href']
     stars = parse_star_count(star_tag.text.strip())
     return username, repo_name, stars, repo_url
-  
+
+# get_repo_info gives us the extracted username, repo_name, starts, repo_url
+def get_topic_repos(topic_doc):#function for h3 class
+    h3_selection_class = "f3 color-fg-muted text-normal lh-condensed"
+    repo_tags = topic_doc.find_all('h3',{'class': h3_selection_class})
+    
 def get_topic_repos(topic_doc):
     # Get the h1 tags containing repo title, repo URL and username
     h1_selection_class = 'f3 color-text-secondary text-normal lh-condensed'
     repo_tags = topic_doc.find_all('h1', {'class': h1_selection_class} )
+    
     # Get star tags
     star_tags = topic_doc.find_all('a', { 'class': 'social-count float-none'})
     
@@ -87,21 +111,31 @@ def get_topic_repos(topic_doc):
         topic_repos_dict['repo_url'].append(repo_info[3])
         
     return pd.DataFrame(topic_repos_dict)
-  
-  def scrape_topic(topic_url, path):
+# get_topic_repos gives the top repos for each topic
+
+def scrape_topic(topic_url, path):
     if os.path.exists(path):
         print("The file {} already exists. Skipping...".format(path))
         return
     topic_df = get_topic_repos(get_topic_page(topic_url))
     topic_df.to_csv(path, index=None)
     
- def scrape_topics_repos():
+    
+def scrape_topics_repos():
     print('Scraping list of topics')
     topics_df = scrape_topics()
     
     os.makedirs('data', exist_ok=True)
     for index, row in topics_df.iterrows():
         print('Scraping top repositories for "{}"'.format(row['title']))
-        scrape_topic(row['url'], 'data/{}.csv'.format(row['title']))
-        
+        scrape_topic(row['url'], 'data/{}.csv'.format(row['title']))    
+# Let's run it to scrape the top repos for the all the topics on the first page of https://github.com/topics
+
 scrape_topics_repos()
+
+
+
+
+
+
+
