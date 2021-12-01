75b0567312fc2f4b634c81cf96be13c027ee4a9a
Brinda3 <88205782+Brinda3@users.noreply.github.com>
2021-12-01 13:17:49 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,107 @@
+import requests
+from bs4 import BeautifulSoup
+# `get_topic_titles` can be used to get the list of titles
+def get_topics_page():
+    # TODO - add comments
+    topics_url = 'https://github.com/topics'
+    response = requests.get(topics_url)
+    if response.status_code != 200:
+        raise Exception('Failed to load page {}'.format(topic_url))
+    doc = BeautifulSoup(response.text, 'html.parser')
+    return doc
+doc = get_topics_page()
+
+def get_topic_titles(doc):
+    selection_class = 'f3 lh-condensed mb-0 mt-1 Link--primary'
+    topic_title_tags = doc.find_all('p', {'class': selection_class})
+    topic_titles = []
+    for tag in topic_title_tags:
+        topic_titles.append(tag.text)
+    return topic_titles
+titles = get_topic_titles(doc)
+def get_topic_descs(doc):
+    desc_selector = 'f5 color-text-secondary mb-0 mt-1'
+    topic_desc_tags = doc.find_all('p', {'class': desc_selector})
+    topic_descs = []
+    for tag in topic_desc_tags:
+        topic_descs.append(tag.text.strip())
+    return topic_descs
+ # Similarly we have defined functions for descriptions and URLs. 
+def get_topic_urls(doc):
+    topic_link_tags = doc.find_all('a', {'class': 'd-flex no-underline'})
+    topic_urls = []
+    base_url = 'https://github.com'
+    for tag in topic_link_tags:
+        topic_urls.append(base_url + tag['href'])
+    return topic_urls
+  
+  
+def scrape_topics():
+    topics_url = 'https://github.com/topics'
+    response = requests.get(topics_url)
+    if response.status_code != 200:
+        raise Exception('Failed to load page {}'.format(topic_url))
+    doc = BeautifulSoup(response.text, 'html.parser')
+    topics_dict = {
+        'title': get_topic_titles(doc),
+        'description': get_topic_descs(doc),
+        'url': get_topic_urls(doc)
+    }
+    return pd.DataFrame(topics_dict)
+  
+ def get_topic_page(topic_url):
+    # Download the page
+    response = requests.get(topic_url)
+    # Check successful response
+    if response.status_code != 200:
+        raise Exception('Failed to load page {}'.format(topic_url))
+    # Parse using Beautiful soup
+    topic_doc = BeautifulSoup(response.text, 'html.parser')
+    return topic_doc
+ doc = get_topic_page('https://github.com/topics/3d')
+
+def get_repo_info(h1_tag, star_tag):
+    # returns all the required info about a repository
+    a_tags = h1_tag.find_all('a')
+    username = a_tags[0].text.strip()
+    repo_name = a_tags[1].text.strip()
+    repo_url =  base_url + a_tags[1]['href']
+    stars = parse_star_count(star_tag.text.strip())
+    return username, repo_name, stars, repo_url
+  
+def get_topic_repos(topic_doc):
+    # Get the h1 tags containing repo title, repo URL and username
+    h1_selection_class = 'f3 color-text-secondary text-normal lh-condensed'
+    repo_tags = topic_doc.find_all('h1', {'class': h1_selection_class} )
+    # Get star tags
+    star_tags = topic_doc.find_all('a', { 'class': 'social-count float-none'})
+    
+    topic_repos_dict = { 'username': [], 'repo_name': [], 'stars': [],'repo_url': []}
+
+    # Get repo info
+    for i in range(len(repo_tags)):
+        repo_info = get_repo_info(repo_tags[i], star_tags[i])
+        topic_repos_dict['username'].append(repo_info[0])
+        topic_repos_dict['repo_name'].append(repo_info[1])
+        topic_repos_dict['stars'].append(repo_info[2])
+        topic_repos_dict['repo_url'].append(repo_info[3])
+        
+    return pd.DataFrame(topic_repos_dict)
+  
+  def scrape_topic(topic_url, path):
+    if os.path.exists(path):
+        print("The file {} already exists. Skipping...".format(path))
+        return
+    topic_df = get_topic_repos(get_topic_page(topic_url))
+    topic_df.to_csv(path, index=None)
+    
+ def scrape_topics_repos():
+    print('Scraping list of topics')
+    topics_df = scrape_topics()
+    
+    os.makedirs('data', exist_ok=True)
+    for index, row in topics_df.iterrows():
+        print('Scraping top repositories for "{}"'.format(row['title']))
+        scrape_topic(row['url'], 'data/{}.csv'.format(row['title']))
+        
+scrape_topics_repos()
